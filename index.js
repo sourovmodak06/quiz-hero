4a31ad4737e4f06797006d408a37b2dc90e368de
sourovmodak06 <sourovmodak650@gmail.com>
2023-03-16 12:44:48 +0600

    blog page question and answer

--- index.js
+++ index.js
@@ -136,52 +136,57 @@ document.querySelector("#submit").addEventListener("click", () => {
    // Right side bar/ answer section
    let x = setTimeout(() => {
     showAnswers(answers);
-    displayResult.innerHTML = `<div
-    class="h-[220px] w-[220px] mx-auto mt-8 flex flex-col justify-center border-2 rounded-tr-[50%] rounded-bl-[50%]"
-  >
-    <h3 class="text-xl ${grade.color}">${grade.status}</h3>
-    <h1 class="text-3xl font-bold my-2">
-      ${totalMark}<span class="text-slate-800">/60</span>
-    </h1>
-    <p class="text-sm flex justify-center items-center gap-2">
-      Total Time: <span class="text-xl text-orange-500">${timeTaken.innerText.replace(
-        "sec",
-        ""
-      )}<span class="text-xs">sec</span></span>
-    </p>
-  </div>
-  
-  <button onclick="location.reload();" class="bg-green-600 text-white w-full py-2 rounded mt-16">Restart</button>
-  ${
-    storage
-      ? `<div class="mt-5">
-      <h1 class="text-center">Previous Submissions <button class="text-blue-800 text-xs" onclick={localStorage.clear();location.reload()}>Clear History</button></h1>
-    <div
-    class="flex justify-between items-center border rounded p-2 my-2 shadow-sm font-medium">
-    <div>Marks</div>
-    <div>Grade</div>
-    <div>Time</div>
-    </div>
-    ${storage
-      ?.reverse()
-      ?.map(
-        (item) => `<div
-      class="flex justify-between items-center border rounded p-2 my-2 shadow-sm">
-      <div>${item.marks}/60</div>
-      <div>${item.status}</div>
-      <div>${item.examTime}</div>
-      </div>`
-      )
-      ?.join("")}`
-      : ""
-  }
-  </div>
-  `;
+    displayResult.innerHTML = 
+    `<div class="h-[220px] w-[220px] mx-auto mt-8 flex flex-col justify-center border-2 rounded-tr-[50%] rounded-bl-[50%]">
+        <h3 class="text-xl ${grade.color}">${grade.status}</h3>
+        <h1 class="text-3xl font-bold my-2">
+          ${totalMark}<span class="text-slate-800">/60</span>
+        </h1>
+        <p class="text-sm flex justify-center items-center gap-2">
+          Total Time: <span class="text-xl text-orange-500">${timeTaken.innerText.replace(
+            "sec",
+            ""
+          )}<span class="text-xs">sec</span></span>
+        </p>
+     </div>
+     
+     <button onclick="location.reload();" class="bg-green-600 text-white w-full py-2 rounded mt-16">Restart</button>
 
-    clearTimeout(x);
+
+
+
+
+     
+     ${storage? 
+      `<div class="mt-5">
+            <h1 class="text-center">Previous Submissions <button class="text-blue-800 text-xs" onclick={localStorage.clear();location.reload()}>Clear History</button></h1>
+            <div class="flex justify-between items-center border rounded p-2 my-2 shadow-sm font-medium">
+            <div>Marks</div>
+            <div>Grade</div>
+            <div>Time</div>
+        </div>
+        ${storage?.reverse()?.map((item) => 
+          `<div class="flex justify-between items-center border rounded p-2 my-2 shadow-sm">
+                <div>${item.marks}/60</div>
+                <div>${item.status}</div>
+                <div>${item.examTime}</div>
+          </div>`
+                )
+                ?.join("")}`
+                : ""}
+          </div>`;
+        clearTimeout(x);
   }, 1500);
+
+
   window.scrollTo(0, 0);
 });
+
+
+
+
+
+
 
 function blogPage(){
   location.href = 'blog.html';
